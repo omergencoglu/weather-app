1f9688dca4e1aa150cdd1a6b903ec58ccfef7c98
Omer Gencoglu <omersaitgencoglu@gmail.com>
2021-11-28 15:52:57 -0500

    Moved gradient variables to css and changed backgtround change functionality to class toggle

--- app.js
+++ app.js
@@ -5,137 +5,37 @@ const conditionImage = document.getElementById("conditionImage");
 const condition = document.getElementById("condition");
 const backgroundGrad = document.getElementById("container");
 
-//backgroud gradients from https://cdpn.io/rDEAl
 const dayGrads = [
-  [
-    { color: "4a4969", position: 0 },
-    { color: "7072ab", position: 50 },
-    { color: "cd82a0", position: 100 },
-  ],
-  [
-    { color: "757abf", position: 0 },
-    { color: "8583be", position: 60 },
-    { color: "eab0d1", position: 100 },
-  ],
-  [
-    { color: "82addb", position: 0 },
-    { color: "ebb2b1", position: 100 },
-  ],
-  [
-    { color: "94c5f8", position: 1 },
-    { color: "a6e6ff", position: 70 },
-    { color: "b1b5ea", position: 100 },
-  ],
-  [
-    { color: "b7eaff", position: 0 },
-    { color: "94dfff", position: 100 },
-  ],
-  [
-    { color: "9be2fe", position: 0 },
-    { color: "67d1fb", position: 100 },
-  ],
-  [
-    { color: "90dffe", position: 0 },
-    { color: "38a3d1", position: 100 },
-  ],
-  [
-    { color: "57c1eb", position: 0 },
-    { color: "246fa8", position: 100 },
-  ],
-  [
-    { color: "2d91c2", position: 0 },
-    { color: "1e528e", position: 100 },
-  ],
-  [
-    { color: "2473ab", position: 0 },
-    { color: "1e528e", position: 70 },
-    { color: "5b7983", position: 100 },
-  ],
-  [
-    { color: "1e528e", position: 0 },
-    { color: "265889", position: 50 },
-    { color: "9da671", position: 100 },
-  ],
-  [
-    { color: "1e528e", position: 0 },
-    { color: "728a7c", position: 50 },
-    { color: "e9ce5d", position: 100 },
-  ],
-  [
-    { color: "154277", position: 0 },
-    { color: "576e71", position: 30 },
-    { color: "e1c45e", position: 70 },
-    { color: "b26339", position: 100 },
-  ],
-  [
-    { color: "163C52", position: 0 },
-    { color: "4F4F47", position: 30 },
-    { color: "C5752D", position: 60 },
-    { color: "B7490F", position: 80 },
-    { color: "2F1107", position: 100 },
-  ],
-  [
-    { color: "071B26", position: 0 },
-    { color: "071B26", position: 30 },
-    { color: "8A3B12", position: 80 },
-    { color: "240E03", position: 100 },
-  ],
+  "day-1",
+  "day-2",
+  "day-3",
+  "day-4",
+  "day-5",
+  "day-6",
+  "day-7",
+  "day-8",
+  "day-9",
+  "day-10",
+  "day-11",
+  "day-12",
+  "day-13",
+  "day-14",
+  "day-15",
 ];
+
 const nightGrads = [
-  [
-    { color: "010A10", position: 30 },
-    { color: "59230B", position: 80 },
-    { color: "2F1107", position: 100 },
-  ],
-  [
-    { color: "090401", position: 50 },
-    { color: "4B1D06", position: 100 },
-  ],
-  [
-    { color: "00000c", position: 80 },
-    { color: "150800", position: 100 },
-  ],
-  [
-    { color: "00000c", position: 0 },
-    { color: "00000c", position: 0 },
-  ],
-  [
-    { color: "020111", position: 85 },
-    { color: "191621", position: 100 },
-  ],
-  [
-    { color: "020111", position: 60 },
-    { color: "20202c", position: 100 },
-  ],
-  [
-    { color: "020111", position: 10 },
-    { color: "3a3a52", position: 100 },
-  ],
-  [
-    { color: "20202c", position: 0 },
-    { color: "515175", position: 100 },
-  ],
-  [
-    { color: "40405c", position: 0 },
-    { color: "6f71aa", position: 80 },
-    { color: "8a76ab", position: 100 },
-  ],
+  "night-1",
+  "night-2",
+  "night-3",
+  "night-4",
+  "night-5",
+  "night-6",
+  "night-7",
+  "night-8",
+  "night-9",
 ];
 
-//create css declaration for background
-const toCSSGradient = (data) => {
-  let css = "linear-gradient(to bottom,";
-  const len = data.length;
-
-  for (let i = 0; i < len; i++) {
-    const item = data[i];
-    css += " #" + item.color + " " + item.position + "%";
-    if (i < len - 1) css += ",";
-  }
-  return `${css} )`;
-};
-
-//transform unixtimestamp to date and return in minute format
+//transform unixtimestamp to date and return it in minute format
 const getTimeInfo = (unixSunrise, unixSunset, unixCurrentTime) => {
   const sunrise = new Date(unixSunrise * 1000);
   const sunriseHour = sunrise.getHours();
@@ -288,9 +188,13 @@ const searchData = () => {
           timeInformation.sunsetHour,
           selectedGrads
         );
+        const backgroundGradClass = backgroundGrad.classList;
 
-        // apply background for day
-        backgroundGrad.style.background = toCSSGradient(
+        backgroundGradClass.remove(
+          `${backgroundGradClass[backgroundGradClass.length - 1]}`
+        );
+
+        backgroundGradClass.add(
           selectGradient(
             timeInformation.currentHour,
             timeDifference,
